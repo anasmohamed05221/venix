4af9d369c0c0c2929031411553f72b8a31d5275a
Anas Mohamed <anasabdelrhman397@gmail.com>
2026-05-22 01:44:23 +0300

    fix: add a default value for resend api key for CI

--- core/config.py
+++ core/config.py
@@ -13,7 +13,7 @@ class Settings(BaseSettings):
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
     REFRESH_TOKEN_EXPIRE_DAYS: int
-    RESEND_API_KEY: str
+    RESEND_API_KEY: str = ""
     MAIL_FROM: str
     LOG_LEVEL: str = "INFO"
     LOG_DIR: str = "logs"
